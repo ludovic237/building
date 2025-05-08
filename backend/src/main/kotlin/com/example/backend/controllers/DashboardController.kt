44a644e2d8258fcfe379d3344f7884584f4fd2a1
Boris <bgomne@gmail.com>
2025-05-08 22:55:14 +0100

    update

--- backend/src/main/kotlin/com/example/backend/controllers/DashboardController.kt
+++ backend/src/main/kotlin/com/example/backend/controllers/DashboardController.kt
@@ -4,6 +4,7 @@ import com.example.backend.dtos.DashboardDTO
 import com.example.backend.services.DashboardService
 import com.example.backend.services.PaymentService
 import com.example.backend.services.UserService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
@@ -28,12 +29,12 @@ class DashboardController(
 
 @GetMapping("/info")
 fun getAdminDashboardData(
-    @RequestParam(required = false) startDate: LocalDateTime?,
-    @RequestParam(required = false) endDate: LocalDateTime?
-): Map<String, Any> {
-    val defaultStartDate = startDate ?: LocalDateTime.now().minusMonths(1) // Par exemple, 1 mois avant aujourd'hui
-    val defaultEndDate = endDate ?: LocalDateTime.now() // Aujourd'hui comme date de fin par défaut
-    return dashboardService!!.getAdminDashboardData(defaultStartDate, defaultEndDate)
-}
+    @RequestParam("startDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") startDate: LocalDateTime,
+    @RequestParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") endDate: LocalDateTime
+  ): Map<String, Any> {
+      val defaultStartDate = startDate
+      val defaultEndDate = endDate
+      return dashboardService!!.getAdminDashboardData(defaultStartDate, defaultEndDate)
+  }
 
 }
--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import {DashboardService} from "@services/dashboard.service";
+// import * as moment from 'moment';
 
 @Component({
     selector: 'app-dashboard',
@@ -35,7 +36,24 @@ export class DashboardComponent {
   }
 
   private fetchDashboardData(): void {
-    this.dashboardService.getDashboards().subscribe(
+    // const startDate = moment().startOf('day').format('YYYY-MM-DDTHH:mm:ss'); // Start of the day at 00:00
+    // const endDate = moment().endOf('day').format('YYYY-MM-DDTHH:mm:ss'); // End of the day at 23:59
+
+    const now = new Date();
+    // const startDate = new Date(now.setHours(0, 0, 0, 0)).toISOString(); // Début de la journée à 00:00
+    // const endDate = new Date(now.setHours(23, 59, 59, 999)).toISOString(); // Fin de la journée à 23:59
+
+    // const startDate = new Date(now.setHours(0, 0, 0, 0)).toISOString().slice(0, -1); // Remove 'Z'
+    // const endDate = new Date(now.setHours(23, 59, 59, 999)).toISOString().slice(0, -1); // Remove 'Z'
+
+    const startDate = new Date(now.setHours(0, 0, 0, 0))
+      .toISOString()
+      .slice(0, 19); // Format as 'yyyy-MM-ddTHH:mm:ss'
+    const endDate = new Date(now.setHours(23, 59, 59, 999))
+      .toISOString()
+      .slice(0, 19); // Format as 'yyyy-MM-ddTHH:mm:ss'
+
+    this.dashboardService.getDashboards(startDate, endDate).subscribe(
       (data) => {
         this.dashboardData = data;
         console.log('Dashboard data:', this.dashboardData);
